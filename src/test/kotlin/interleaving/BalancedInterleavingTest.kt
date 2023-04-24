13d816e3353eeedf5deb9e709cb4dc8262c12602
ryook <ryo.kato23@gmail.com>
2023-04-24 22:10:46 +0900

    add BalancedInterleavingTest

--- src/test/kotlin/interleaving/BalancedInterleavingTest.kt
+++ src/test/kotlin/interleaving/BalancedInterleavingTest.kt
@@ -1,6 +1,6 @@
 package interleaving
 
-import interleaving.data.Ranking
+import interleaving.data.RankingItem
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -8,26 +8,51 @@ class BalancedInterleavingTest {
 
     @Test
     fun interleave() {
-        val rankingA = generateRanking(listOf("1", "2", "3"))
-        val rankingB = generateRanking(listOf("4", "5", "6"))
+        val rankingA = generateRanking(listOf("1", "2", "3"), "rankingA")
+        val rankingB = generateRanking(listOf("4", "5", "6"), "rankingB")
 
         val actual = BalancedInterleaving<String>().interleave(rankingA = rankingA, rankingB = rankingB)
 
-        assertEquals(listOf("1", "5", "6"), actual)
+        val expected = listOf(
+            RankingItem(0, "1", item = "1", ranking = "rankingA"),
+            RankingItem(1, "2", item = "2", ranking = "rankingB"),
+            RankingItem(2, "3", item = "3", ranking = "rankingA"),
+        )
+        assertEquals(expected, actual)
     }
 
     @Test
     fun interleaveSameItems() {
-        val rankingA = generateRanking(listOf("1", "2", "3"))
-        val rankingB = generateRanking(listOf("1", "2", "3"))
+        val rankingA = generateRanking(listOf("1", "2", "3"), "rankingA")
+        val rankingB = generateRanking(listOf("1", "2", "3"), "rankingB")
 
         val actual = BalancedInterleaving<String>().interleave(rankingA = rankingA, rankingB = rankingB)
 
-        assertEquals(listOf("1", "2", "3"), actual)
+        val expected = listOf(
+            RankingItem(0, "1", item = "1", ranking = "rankingA"),
+            RankingItem(1, "2", item = "2", ranking = "rankingB"),
+            RankingItem(2, "3", item = "3", ranking = "rankingA"),
+        )
+        assertEquals(expected, actual)
     }
 
-    private fun generateRanking(values: List<String>): List<Ranking<String>> {
-        return values.mapIndexed { index, s -> Ranking(index = index, value = s, item = s) }
+    @Test
+    fun interleave2() {
+        val rankingA = generateRanking(listOf("1", "2", "3"), "rankingA")
+        val rankingB = generateRanking(listOf("2", "1", "3"), "rankingB")
+
+        val actual = BalancedInterleaving<String>().interleave(rankingA = rankingA, rankingB = rankingB)
+
+        val expected = listOf(
+            RankingItem(0, "1", item = "1", ranking = "rankingA"),
+            RankingItem(1, "2", item = "2", ranking = "rankingA"),
+            RankingItem(2, "3", item = "3", ranking = "rankingA"),
+        )
+        assertEquals(expected, actual)
+    }
+
+    private fun generateRanking(values: List<String>, rankingSource: String): List<RankingItem<String>> {
+        return values.mapIndexed { index, s -> RankingItem(index = index, key = s, item = s, ranking = rankingSource) }
     }
 
 }
